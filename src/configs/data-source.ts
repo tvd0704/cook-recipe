4b05c2201a92fe2840521f295c4fbb12192db689
tvd0704 <85782671+tvd0704@users.noreply.github.com>
2023-11-01 17:10:00 +0700

    Update data-source.ts

--- src/configs/data-source.ts
+++ src/configs/data-source.ts
@@ -4,15 +4,16 @@ import { entities } from "../domains/entities";
 
 export const dataSource = new DataSource({
   type: "mysql",
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT as any,
-  username: process.env.DB_USERNAME,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  host: "sql.freedb.tech",
+  port: 3306,
+  username: "freedb_tvd-user",
+  password: "?Sy?UQm4CBhdVw?",
+  database: "freedb_cookdb",
   synchronize: true,
   logging: false,
   entities,
 });
+
 
 dataSource
   .initialize()
